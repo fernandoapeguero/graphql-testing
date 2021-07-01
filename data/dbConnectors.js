bcd6e6c9b23ee8b7b4a6be871fb7b949b80ab71c
fernandopeguero <fernandoapeguero@gmail.com>
2021-07-01 16:10:08 -0400

    implemented random data into the database and added the aliens query to the schema

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import Sequelize from 'sequelize';
-import _ from 'lodash';
+import _, { last } from 'lodash';
 import casual from 'casual';
 
 // Mongo Connection
@@ -49,6 +49,16 @@ const Aliens = sequelize.define('aliens', {
     firstName: { type: Sequelize.STRING },
     lastName: { type: Sequelize.STRING },
     planet: { type: Sequelize.STRING },
+});
+
+Aliens.sync({ force: true }).then(() => {
+    _.times(10, (i) => {
+        Aliens.create({
+            firstName: casual.first_name,
+            lastName: casual.last_name,
+            planet: casual.word,
+        });
+    });
 });
 
 export { Friends, Aliens };
